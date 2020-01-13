7d3cdaeb08b649b71e0c0f5b21313aad2860d6bd
mvaughan <magnus.vaughan@gmail.com>
2020-01-13 07:44:20 +0000

    Added Posts controller / model / migration + created resourceful routes

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/posts', 'PostController@index')->name('posts.index');
+Route::get('/posts/create', 'PostController@create')->name('posts.create');
+Route::post('/posts', 'PostController@store')->name('posts.store');
+Route::get('/posts/{post}', 'PostController@show')->name('posts.show');
+Route::get('/posts/{post}/edit', 'PostController@edit')->name('posts.edit');
+Route::put('/posts/{post}', 'PostController@update')->name('posts.update');
+Route::delete('/posts/{post}', 'PostController@destroy')->name('posts.destroy');
+
 
